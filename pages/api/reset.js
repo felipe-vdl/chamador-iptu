64cb149a3b6ba02aaee3d971538053a67965353e
Felipe Vidal <felipe.vidal.mesquita@gmail.com>
2023-01-12 13:46:24 -0300

    reset to 1

--- pages/api/reset.js
+++ pages/api/reset.js
@@ -8,10 +8,7 @@ export default async function Reset(req, res) {
 
   await prisma.password.deleteMany({});
   await prisma.$queryRaw`TRUNCATE TABLE password;`;
-  await prisma.password.create({ data: { id: 1 } });
-  await prisma.totalPasswords.update({
-    where: { id: 1 }, data: { count: { increment: 1 } }
-  })
+  // await prisma.password.create({ data: { id: 1 } });
 
   res.status(200).json({ id: 1 });
 }
