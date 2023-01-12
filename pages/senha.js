812c9d2bc5f802d41eca3582aef5b904ed75335d
Felipe Vidal <felipe.vidal.mesquita@gmail.com>
2023-01-12 14:34:18 -0300

    Styles

--- pages/senha.js
+++ pages/senha.js
@@ -33,16 +33,16 @@ export default function Senha() {
   return (
     <div className='flex flex-col h-screen'>
       <img src={mainLogo.src} className="self-center sm:self-center md:self-start mb-3 mt-5 ml-5 w-[250px]" />
-      <h1 className='text-3xl mt-auto mb-5 text-center'>Clique no botão para imprimir uma senha</h1>
+      <h1 className='self-center text-3xl mt-auto mb-5 text-center shadow px-12 py-2 bg-blue-100'>Clique no botão para imprimir uma senha</h1>
       <button
         to={`/senha`}
         onClick={handleGerarSenha}
-        className="self-center text-white mb-auto font-bold disabled:bg-indigo-300 disabled:text-slate-100 p-2 bg-indigo-800 rounded hover:bg-blue-900 text-[86px] px-8 hover:text-blue-100"
+        className="shadow-lg shadow-indigo-500 hover:shadow-indigo-600 self-center text-white mb-auto font-bold hover:shadow-blue-500 disabled:bg-indigo-300 disabled:text-slate-100 active:bg-blue-700 p-2 bg-indigo-800 rounded hover:scale-105 transition hover:bg-blue-900 text-[86px] px-8 hover:text-blue-100"
         disabled={isLoading}
       >
         {!isLoading ? "Imprimir" : "Imprimindo..."}
       </button>
-      <footer className='mt-auto bg-indigo-800 p-1 text-indigo-200 text-white text-xs text-center'>2023 © Subsecretaria de Tecnologia da Informação — Prefeitura Municipal de Mesquita</footer>
+      <footer className='shadow-inner shadow-indigo-800 mt-auto bg-indigo-800 p-1 text-indigo-200 text-white text-xs text-center'>2023 © Subsecretaria de Tecnologia da Informação — Prefeitura Municipal de Mesquita</footer>
     </div>
   );
 }
